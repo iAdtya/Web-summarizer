ecf0cdce3369e655dc64108e598d664572c34f97
Aditya Khedekar <adityakhedekar98906@gmail.com>
2024-01-10 21:58:12 +0530

    Remove Redis client and update dependencies

--- src/controllers/scrape_web_page.ts
+++ src/controllers/scrape_web_page.ts
@@ -1,6 +1,5 @@
 import puppeteer from "puppeteer";
 import { summerizeWebPage } from "./summarize_web_page";
-import client from "../redisClient";
 
 export const scrapeWebPage = async (url: string) => {
   const browser = await puppeteer.launch({ headless: "new" });
@@ -12,20 +11,8 @@ export const scrapeWebPage = async (url: string) => {
   const content = await page.evaluate(() => document.body.outerHTML);
   await browser.close();
 
-  // const cachedSummary = await new Promise<string | null>((resolve, reject) => {
-  //   client.get(url, (error: Error | null, result: string | null) => {
-  //     if (error) reject(error);
-  //     resolve(result);
-  //   });
-  // });
-
-  // if (cachedSummary) {
-  //   return JSON.parse(cachedSummary);
-  // }
-
   const summary = await summerizeWebPage(content);
 
   //cache the summary with the url as the key
-  // client.set(url, JSON.stringify(summary));
   return summary;
 };
